1fc1cb5729d900344d02bb7f71fdd04f4bb8c633
Aldot-02 <twizald.02@gmail.com>
2023-10-19 07:55:37 +0000

    0x02-redis_basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,21 +1,54 @@
-#!/usr/bin/env python3
-"""
-create a web cach
-"""
-import redis
 import requests
-rc = redis.Redis()
-count = 0
+import time
+from functools import wraps
 
+# Cache to store results with expiration time
+cache = {}
 
-def get_page(url: str) -> str:
-    """ get a page and cach value"""
-    rc.set(f"cached:{url}", count)
-    resp = requests.get(url)
-    rc.incr(f"count:{url}")
-    rc.setex(f"cached:{url}", 10, rc.get(f"cached:{url}"))
-    return resp.text
+def get_page(url):
+    cache_key = f"count:{url}"
+    
+    # Check if the result is already in cache
+    if cache.get(cache_key) and time.time() - cache[cache_key]['timestamp'] < 10:
+        cache[cache_key]['count'] += 1
+        return cache[url]['content']
+    
+    # If not in cache, fetch the page
+    response = requests.get(url)
+    content = response.text
+    
+    # Update cache with new result
+    cache[cache_key] = {
+        'content': content,
+        'timestamp': time.time(),
+        'count': 1
+    }
+    
+    return content
 
+# Bonus: Implementing a decorator for caching
+def cache_decorator(func):
+    cache = {}
+    
+    @wraps(func)
+    def wrapper(url):
+        cache_key = f"count:{url}"
+        
+        if cache.get(cache_key) and time.time() - cache[cache_key]['timestamp'] < 10:
+            cache[cache_key]['count'] += 1
+            return cache[url]['content']
+        
+        response = func(url)
+        cache[cache_key] = {
+            'content': response,
+            'timestamp': time.time(),
+            'count': 1
+        }
+        
+        return response
+    
+    return wrapper
 
-if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+@cache_decorator
+def get_page_with_cache(url):
+    return requests.get(url).text
