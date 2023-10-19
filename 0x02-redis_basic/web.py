85c0bbd5609cfbdb57f03304454a822fba76194c
Aldot-02 <twizald.02@gmail.com>
2023-10-19 08:04:40 +0000

    0x02-redis_basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,54 +1,36 @@
+#!/usr/bin/env python3
+"""
+Caching request module
+"""
+import redis
 import requests
-import time
 from functools import wraps
+from typing import Callable
 
-# Cache to store results with expiration time
-cache = {}
 
-def get_page(url):
-    cache_key = f"count:{url}"
-    
-    # Check if the result is already in cache
-    if cache.get(cache_key) and time.time() - cache[cache_key]['timestamp'] < 10:
-        cache[cache_key]['count'] += 1
-        return cache[url]['content']
-    
-    # If not in cache, fetch the page
-    response = requests.get(url)
-    content = response.text
-    
-    # Update cache with new result
-    cache[cache_key] = {
-        'content': content,
-        'timestamp': time.time(),
-        'count': 1
-    }
-    
-    return content
-
-# Bonus: Implementing a decorator for caching
-def cache_decorator(func):
-    cache = {}
-    
-    @wraps(func)
-    def wrapper(url):
-        cache_key = f"count:{url}"
-        
-        if cache.get(cache_key) and time.time() - cache[cache_key]['timestamp'] < 10:
-            cache[cache_key]['count'] += 1
-            return cache[url]['content']
-        
-        response = func(url)
-        cache[cache_key] = {
-            'content': response,
-            'timestamp': time.time(),
-            'count': 1
-        }
-        
+def track_get_page(fn: Callable) -> Callable:
+    """ Decorator for get_page
+    """
+    @wraps(fn)
+    def wrapper(url: str) -> str:
+        """ Wrapper that:
+            - check whether a url's data is cached
+            - tracks how many times get_page is called
+        """
+        client = redis.Redis()
+        client.incr(f'count:{url}')
+        cached_page = client.get(f'{url}')
+        if cached_page:
+            return cached_page.decode('utf-8')
+        response = fn(url)
+        client.set(f'{url}', response, 10)
         return response
-    
     return wrapper
 
-@cache_decorator
-def get_page_with_cache(url):
-    return requests.get(url).text
+
+@track_get_page
+def get_page(url: str) -> str:
+    """ Makes a http request to a given endpoint
+    """
+    response = requests.get(url)
+    return response.text
